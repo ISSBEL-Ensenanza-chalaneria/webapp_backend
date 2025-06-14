daf3e0134d23841cc772c69d96898eaed156c745
Juan David Muñoz Pasquel <jmunozpasquel@gmail.com>
2025-06-13 23:20:58 -0500

    Se elimina lo relacionado a wordpress Se crea perfil application-prod Se configura cors

--- core/src/main/java/com/wordpress/issbelensenanzachalaneria/webapp/core/infrastructure/config/Securityconfig.java
+++ core/src/main/java/com/wordpress/issbelensenanzachalaneria/webapp/core/infrastructure/config/Securityconfig.java
@@ -1,13 +1,18 @@
 package com.wordpress.issbelensenanzachalaneria.webapp.core.infrastructure.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,13 +25,26 @@ public class Securityconfig {
         http.csrf(csrf -> csrf.disable()).cors(Customizer.withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(request -> request.requestMatchers(PUBLIC_URL).permitAll());
-        autorizeWordPressContent(http);
         return http.build();
     }
 
-    private void autorizeWordPressContent(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(
-            request -> request.requestMatchers(HttpMethod.GET, "/api/wordpress/**").permitAll()
-        );
+    @Bean
+    CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", ""));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin",
+                "Content-Type",
+                "Accept", "Jwt-Token", "Authorization", "Origin", "Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration
+                .setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token",
+                        "Authorization",
+                        "Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Credentials", "File-Name"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 }
