c8186f23952c5e45682519d3b9ed6e9b85e89c78
Juan David Muñoz Pasquel <jmunozpasquel@gmail.com>
2025-06-13 23:22:29 -0500

    Limpieza de endpoints en el cors. Se configuran más adelante Variables locales de render para datos de conexión

--- core/src/main/java/com/wordpress/issbelensenanzachalaneria/webapp/core/infrastructure/config/Securityconfig.java
+++ core/src/main/java/com/wordpress/issbelensenanzachalaneria/webapp/core/infrastructure/config/Securityconfig.java
@@ -33,7 +33,7 @@ public class Securityconfig {
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", ""));
+        corsConfiguration.setAllowedOrigins(List.of("", ""));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin",
                 "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin", "Accept", "X-Requested-With",
