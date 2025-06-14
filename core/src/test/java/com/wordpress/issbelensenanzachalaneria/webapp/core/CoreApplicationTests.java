53f875a04ebb1ce7bed4339cd97760cf4ad0f0f0
Juan David Muñoz Pasquel <jmunozpasquel@gmail.com>
2025-06-14 03:51:56 -0500

    Se ajusta el userDto Se prepara el proyecto para despliegue

--- core/src/test/java/com/wordpress/issbelensenanzachalaneria/webapp/core/CoreApplicationTests.java
+++ core/src/test/java/com/wordpress/issbelensenanzachalaneria/webapp/core/CoreApplicationTests.java
@@ -1,13 +1,9 @@
 package com.wordpress.issbelensenanzachalaneria.webapp.core;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
