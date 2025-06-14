25053ef679313802541860453888dd8395f7cc5d
Juan David Muñoz Pasquel <jmunozpasquel@gmail.com>
2025-06-14 05:24:37 -0500

    Ignoring createdAt and updatedAt whit mapstruct (dto to entity)

--- core/src/main/java/com/wordpress/issbelensenanzachalaneria/webapp/core/infrastructure/mappers/UserMapstructMapper.java
+++ core/src/main/java/com/wordpress/issbelensenanzachalaneria/webapp/core/infrastructure/mappers/UserMapstructMapper.java
@@ -2,7 +2,6 @@ package com.wordpress.issbelensenanzachalaneria.webapp.core.infrastructure.mappe
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import com.wordpress.issbelensenanzachalaneria.webapp.core.domain.users.User;
 import com.wordpress.issbelensenanzachalaneria.webapp.core.domain.users.UserDto;
@@ -11,9 +10,7 @@ import com.wordpress.issbelensenanzachalaneria.webapp.core.domain.users.UserDto;
 public interface UserMapstructMapper {
     UserDto fromEntity(User user);
 
-    @Mappings({
-            @Mapping(target = "createdAt", ignore = true),
-            @Mapping(target = "updatedAt", ignore = true)
-    })
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     User toEntity(UserDto dto);
 }
