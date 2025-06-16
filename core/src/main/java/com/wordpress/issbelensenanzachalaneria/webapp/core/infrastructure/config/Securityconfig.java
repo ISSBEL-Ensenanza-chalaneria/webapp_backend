d31df71d90702089bf1705b2562474ec9c5ec34a
unknown <mariaquijano@unicomfacauca.edu.co>
2025-06-15 23:04:14 -0500

    Fixed cors path

--- core/src/main/java/com/wordpress/issbelensenanzachalaneria/webapp/core/infrastructure/config/Securityconfig.java
+++ core/src/main/java/com/wordpress/issbelensenanzachalaneria/webapp/core/infrastructure/config/Securityconfig.java
@@ -20,7 +20,7 @@ public class Securityconfig {
     private static final String[] PUBLIC_URL = {
         "/api/users/",
         "/api/users",
-        "/api/users/**"
+        "/api/users/**",
     };
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,7 +35,7 @@ public class Securityconfig {
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "https://issbel-ensenanza-chalaneria.github.io/web_app"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "https://issbel-ensenanza-chalaneria.github.io"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin",
                 "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin", "Accept", "X-Requested-With",
